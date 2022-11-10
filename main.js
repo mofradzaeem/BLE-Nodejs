a61970433018edc7b866b61a54586c6e6da1787a
Greg Wilding <gregory.wilding@ucalgary.ca>
2022-11-10 16:38:21 -0700

    Removing some dead code

--- main.js
+++ main.js
@@ -12,9 +12,6 @@ function createWindow() {
    const win = new BrowserWindow({
       width: 800,
       height: 600,
-      webPreferences: {
-         preload: path.join(__dirname, 'preload.js')
-      }
    });
 
    // Device scans start with calling Bluetooth.requestDevice(...) on the renderer side.  
