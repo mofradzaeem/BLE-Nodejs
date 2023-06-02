06994b465c2135d47e046acb11d6e2534a3d00ea
mofradzaeem <mofradhaque@gmail.com>
2023-06-02 10:52:44 -0600

    changes

--- main.js
+++ main.js
@@ -1,32 +1,73 @@
-// This application demonstrates how to use the Web Bluetooth API
-// it was adapted from: https://github.com/electron/electron/tree/main/docs/fiddles/features/web-bluetooth
-
-"use strict";
-
-const { strict, strictEqual } = require('assert');
-const { app, BrowserWindow } = require('electron');
+const noble = require('noble');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 
+let SERVICE_UUID = '4fafc2011fb5459e8fccc5c9c331914b';
+let CHARACTERISTIC_UUID = 'beb5483e36e14688b7f5ea07361b26a8';
 
 function createWindow() {
    const win = new BrowserWindow({
       width: 800,
       height: 600,
-   });
-
-   // Device scans start with calling Bluetooth.requestDevice(...) on the renderer side.  
-   // When devices are found, this event is triggered.  We can use this event to present 
-   // a device picker to the user, or we can automaically pick something.
-   win.webContents.on('select-bluetooth-device', (event, devices, callback) => {
-      event.preventDefault()
-
-      // just pick the first device discovered (requestDevice() handles filtering)
-      if (devices && devices.length > 0) {
-         callback(devices[0].deviceId)
+      webPreferences: {
+         nodeIntegration: true,
+         contextIsolation: false
       }
    });
 
    win.loadFile('index.html');
+   
+   ipcMain.on('connect-to-device', async (event, arg) => {
+      noble.on('stateChange', function(state) {
+         if (state === 'poweredOn') {
+            noble.startScanning([SERVICE_UUID], false);
+         }
+         else {
+            noble.stopScanning();
+         }
+      });
+   
+      noble.on('discover', function(peripheral) {
+         if (peripheral.advertisement.serviceUuids.includes(SERVICE_UUID)) {
+            noble.stopScanning();
+   
+            peripheral.connect(function(error) {
+               if (error) {
+                  console.log("Connection Error: ", error);
+                  return;
+               }
+   
+               peripheral.discoverServices([SERVICE_UUID], function(error, services) {
+                  const deviceService = services[0];
+                  deviceService.discoverCharacteristics([CHARACTERISTIC_UUID], function(error, characteristics) {
+                     if (error) {
+                        console.log("Characteristic Error: ", error);
+                        return;
+                     }
+   
+                     let deviceCharacteristic = characteristics[0];
+                     deviceCharacteristic.read((error, data) => {
+                        if (error) {
+                           console.log("Read Error: ", error);
+                           return;
+                        }
+                        let value = data.readUInt8(0);
+                        value++; 
+                        let buffer = Buffer.from([value]);
+                        deviceCharacteristic.write(buffer, false, (error) => {
+                           if (error) {
+                              console.log("Write Error: ", error);
+                              return;
+                           }
+                           event.reply('device-data', value.toString(16));
+                        });
+                     });
+                  });
+               });
+            });
+         }
+      });
+   });
 }
 
 app.whenReady().then(() => {
@@ -43,5 +84,3 @@ app.on('window-all-closed', function () {
    console.log("Bye");
    app.quit();
 });
-
-
