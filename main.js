09756bd19e3e8b435b8f0b8d30479291854223b2
mofradzaeem <mofradhaque@gmail.com>
2023-06-16 10:03:47 -0600

    updated issues

--- main.js
+++ main.js
@@ -4,83 +4,66 @@ const path = require('path');
 
 let SERVICE_UUID = '4fafc2011fb5459e8fccc5c9c331914b';
 let CHARACTERISTIC_UUID = 'beb5483e36e14688b7f5ea07361b26a8';
+let peripheralId = '';
+
+noble.on('stateChange', function (state) {
+    if (state === 'poweredOn' && peripheralId !== '') {
+        noble.startScanning([SERVICE_UUID], false);
+    } else {
+        noble.stopScanning();
+    }
+});
+
+noble.on('discover', function (peripheral) {
+    if (peripheral.id === peripheralId) {
+        noble.stopScanning();
+
+        peripheral.connect(function (error) {
+            console.log('Connected to device');
+
+            peripheral.discoverServices([SERVICE_UUID], function (error, services) {
+                var service = services[0];
+                service.discoverCharacteristics([CHARACTERISTIC_UUID], function (error, characteristics) {
+                    var characteristic = characteristics[0];
+                    console.log('Discovered characteristic');
+
+                });
+            });
+        });
+    }
+});
 
 function createWindow() {
-   const win = new BrowserWindow({
-      width: 800,
-      height: 600,
-      webPreferences: {
-         nodeIntegration: true,
-         contextIsolation: false
-      }
-   });
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
 
-   win.loadFile('index.html');
-   
-   ipcMain.on('connect-to-device', async (event, arg) => {
-      noble.on('stateChange', function(state) {
-         if (state === 'poweredOn') {
+    win.loadFile('index.html');
+
+    ipcMain.on('connect-to-device', async (event, arg) => {
+        peripheralId = arg;
+        if(noble.state === 'poweredOn') {
             noble.startScanning([SERVICE_UUID], false);
-         }
-         else {
-            noble.stopScanning();
-         }
-      });
-   
-      noble.on('discover', function(peripheral) {
-         if (peripheral.advertisement.serviceUuids.includes(SERVICE_UUID)) {
-            noble.stopScanning();
-   
-            peripheral.connect(function(error) {
-               if (error) {
-                  console.log("Connection Error: ", error);
-                  return;
-               }
-   
-               peripheral.discoverServices([SERVICE_UUID], function(error, services) {
-                  const deviceService = services[0];
-                  deviceService.discoverCharacteristics([CHARACTERISTIC_UUID], function(error, characteristics) {
-                     if (error) {
-                        console.log("Characteristic Error: ", error);
-                        return;
-                     }
-   
-                     let deviceCharacteristic = characteristics[0];
-                     deviceCharacteristic.read((error, data) => {
-                        if (error) {
-                           console.log("Read Error: ", error);
-                           return;
-                        }
-                        let value = data.readUInt8(0);
-                        value++; 
-                        let buffer = Buffer.from([value]);
-                        deviceCharacteristic.write(buffer, false, (error) => {
-                           if (error) {
-                              console.log("Write Error: ", error);
-                              return;
-                           }
-                           event.reply('device-data', value.toString(16));
-                        });
-                     });
-                  });
-               });
-            });
-         }
-      });
-   });
+        }
+    });
 }
 
 app.whenReady().then(() => {
-   createWindow();
+    createWindow();
 
-   app.on('activate', function () {
-      if (BrowserWindow.getAllWindows().length === 0) {
-         createWindow();
-      }
-   });
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
 });
 
 app.on('window-all-closed', function () {
-   console.log("Bye");
-   app.quit();
+    console.log("Bye");
+    app.quit();
 });
